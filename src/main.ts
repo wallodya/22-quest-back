5d7b6eef23061595ed02a5ef652d6469c8335637
wallodya <78960605+wallodya@users.noreply.github.com>
2023-01-29 14:33:44 +0400

    test:PORT env var

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { AppModule } from "./app.module";
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.use(cookieParser());
-    await app.listen(process.env.PORT_MAIN);
+    await app.listen(process.env.PORT);
     // await app.listen(3000);
 }
 bootstrap();
