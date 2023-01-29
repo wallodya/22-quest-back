3eaae952d9324f23fbd1de1c666018f810dc02fc
wallodya <78960605+wallodya@users.noreply.github.com>
2023-01-29 14:58:20 +0400

    feat:added IP in app.listen

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { Logger } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import * as cookieParser from "cookie-parser";
 import { AppModule } from "./app.module";
@@ -6,8 +7,10 @@ const { PORT } = process.env;
 const LOCALHOST_IP = "0.0.0.0";
 
 async function bootstrap() {
+    const logger = new Logger(bootstrap.name);
     const app = await NestFactory.create(AppModule);
     app.use(cookieParser());
     await app.listen(PORT, LOCALHOST_IP);
+    logger.log(`App is listening on ${LOCALHOST_IP}:${PORT}`);
 }
 bootstrap();
