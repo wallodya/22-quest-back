3bc618a84139f9ebd06b9716e8fb64b279ee724b
wallodya <78960605+wallodya@users.noreply.github.com>
2023-04-15 19:16:41 +0300

    upd:added vercel url into cors

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,10 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.use(cookieParser());
     app.enableCors({
-        origin: "http://localhost:3000",
+        origin: [
+            "http://localhost:3000",
+            "https://todo-proj-93os08ad1-wallodya.vercel.app",
+        ],
         exposedHeaders: ["Authorization"],
         allowedHeaders: [
             "Authorization",
